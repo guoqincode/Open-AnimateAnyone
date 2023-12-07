1278a95a810c1aca04069c8f6d86c60d5fed7351
qguoxdu <55274489+guoqincode@users.noreply.github.com>
2023-12-08 00:25:05 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Unofficial Implementation of Animate Anyone
+
+## Overview
+This repository contains an simple and unofficial implementation of "Animate Anyone". This project is built upon the concepts and initial work found at [magic-animate](https://github.com/magic-research/magic-animate/tree/main).
+
+## To-Do
+- [ ] **Release Training and Inference Code**: Plans are in place to release the training and inference code for the unofficial implementation, along with the unofficial pre-trained weights.
+- [ ] **Data Collection and Release (Within Legal Boundaries)**: Efforts to collect and refine the dataset for further training and improvements are ongoing, with the intention to release it publicly, adhering to legal constraints.
+
+## Acknowledgements
+Special thanks to the original authors of the "Animate Anyone" project and the contributors to the [magic-animate](https://github.com/magic-research/magic-animate/tree/main) repository for their open research and foundational work that inspired this unofficial implementation.
---
+++ models/PoseGuider.py
@@ -0,0 +1,68 @@
+import os
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+from einops import rearrange
+
+
+class PoseGuider(nn.Module):
+    def __init__(self, noise_latent_channels):
+        super(PoseGuider, self).__init__()
+
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU()
+        )
+
+        # Final projection layer
+        self.final_proj = nn.Conv2d(in_channels=128, out_channels=noise_latent_channels, kernel_size=1)
+
+        # Initialize layers
+        self._initialize_weights()
+
+    def _initialize_weights(self):
+        # Initialize weights with Gaussian distribution and zero out the final layer
+        for m in self.conv_layers:
+            if isinstance(m, nn.Conv2d):
+                init.normal_(m.weight, mean=0.0, std=0.02)
+                if m.bias is not None:
+                    init.zeros_(m.bias)
+
+        init.zeros_(self.final_proj.weight)
+        if self.final_proj.bias is not None:
+            init.zeros_(self.final_proj.bias)
+
+    def forward(self, pose_image):
+        original_shape = pose_image.shape
+        if len(original_shape) == 5:
+            # if input video
+            pose_image = rearrange(pose_image, 'b f c h w -> (b f) c h w')
+
+        x = self.conv_layers(pose_image)
+        x = self.final_proj(x)
+
+        if len(original_shape) == 5:
+            x = rearrange(x, '(b f) c h w -> b f c h w', b=original_shape[0], f=original_shape[1])
+
+        return x
+
+    @classmethod
+    def from_pretrained(pretrained_model_path):
+        if not os.path.exists(pretrained_model_path):
+            print(f"There is no model file in {pretrained_model_path}")
+        print(f"loaded PoseGuider's pretrained weights from {pretrained_model_path} ...")
+
+        state_dict = torch.load(pretrained_model_path, map_location="cpu")
+        model = PoseGuider(noise_latent_channels=4)
+        m, u = model.load_state_dict(state_dict, strict=False)
+        print(f"### missing keys: {len(m)}; \n### unexpected keys: {len(u)};")        
+        params = [p.numel() if "temporal" in n else 0 for n, p in model.named_parameters()]
+        print(f"### PoseGuider's Parameters: {sum(params) / 1e6} M")
+        
+        return model
