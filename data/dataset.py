eb78ca6ee8e1a7de3995860a70c056297a0ca048
guoqincode <guoqin@qguo-amd.localdomain>
2023-12-11 20:33:35 +0800

    add ucb dataset

--- data/dataset.py
+++ data/dataset.py
@@ -17,6 +17,7 @@ import torch.distributed as dist
 def zero_rank_print(s):
     if (not dist.is_initialized()) and (dist.is_initialized() and dist.get_rank() == 0): print("### " + s)
 
+
 class TikTok(Dataset):
     def __init__(
             self,
@@ -124,6 +125,119 @@ class TikTok(Dataset):
         
         return sample
 
+class UBC_Fashion(Dataset):
+    def __init__(
+            self,
+            csv_path, video_folder,
+            sample_size=768, sample_stride=4, sample_n_frames=24,
+            is_image=False, clip_model_path="openai/clip-vit-base-patch32",
+            is_train=True,
+        ):
+        zero_rank_print(f"loading annotations from {csv_path} ...")
+        with open(csv_path, 'r') as csvfile:
+            self.dataset = list(csv.DictReader(csvfile))
+        self.length = len(self.dataset)
+        zero_rank_print(f"video nums: {self.length}")
+
+        self.video_folder    = video_folder
+        self.sample_stride   = sample_stride
+        self.sample_n_frames = sample_n_frames
+        self.is_image        = is_image
+        
+        self.is_train = is_train
+        self.spilt = 'train' if self.is_train else 'test'
+        
+        self.clip_image_processor = CLIPProcessor.from_pretrained(clip_model_path,local_files_only=True)
+
+        sample_size = tuple(sample_size) if not isinstance(sample_size, int) else (sample_size, sample_size)
+        self.pixel_transforms = transforms.Compose([
+            # transforms.RandomHorizontalFlip(),
+            transforms.Resize(sample_size[0]),
+            transforms.CenterCrop(sample_size),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], inplace=True),
+        ])
+
+    def __len__(self):
+        return self.length
+    
+    def get_batch(self,idx):
+        video_dict = self.dataset[idx]
+        folder_id, folder_name = video_dict['folder_id'], video_dict['folder_name']
+        
+        video_dir    =    os.path.join(self.video_folder, self.spilt, f"{folder_name}.mp4")
+        video_pose_dir =  os.path.join(self.video_folder, self.spilt+"_dwpose", f"{folder_name}.mp4")
+        
+        video_reader = VideoReader(video_dir)
+        video_reader_pose = VideoReader(video_pose_dir)
+        
+        
+        assert len(video_reader) == len(video_reader_pose), f"len(video_reader) != len(video_reader_pose) in video {idx}"
+        
+        video_length = len(video_reader)
+        
+        if not self.is_image:
+            clip_length = min(video_length, (self.sample_n_frames - 1) * self.sample_stride + 1)
+            start_idx   = random.randint(0, video_length - clip_length)
+            batch_index = np.linspace(start_idx, start_idx + clip_length - 1, self.sample_n_frames, dtype=int)
+        else:
+            batch_index = [random.randint(0, video_length - 1)]
+            
+        pixel_values = torch.from_numpy(video_reader.get_batch(batch_index).asnumpy()).permute(0, 3, 1, 2).contiguous()
+        pixel_values = pixel_values / 255.
+        # del video_reader
+        
+        pixel_values_pose = torch.from_numpy(video_reader_pose.get_batch(batch_index).asnumpy()).permute(0, 3, 1, 2).contiguous()
+        pixel_values_pose = pixel_values_pose / 255.
+        del video_reader_pose
+        
+        if self.is_image:
+            pixel_values = pixel_values[0]
+            pixel_values_pose = pixel_values_pose[0]
+        
+        ref_img_idx = random.randint(0, video_length - 1)
+        ref_img = video_reader[ref_img_idx]
+        ref_img_pil = Image.fromarray(ref_img.asnumpy())
+        
+        clip_ref_image = self.clip_image_processor(images=ref_img_pil, return_tensors="pt").pixel_values
+        
+        pixel_values_ref_img = torch.from_numpy(ref_img.asnumpy()).permute(2, 0, 1).contiguous()
+        pixel_values_ref_img = pixel_values_ref_img / 255.
+        del video_reader
+        
+        # pixel_values: train objective
+        # pixel_values_pose: corresponding pose
+        # clip_ref_image: processed reference clip image
+        # pixel_values_ref_img: ReferenceNet image
+        return pixel_values, pixel_values_pose, clip_ref_image, pixel_values_ref_img
+    
+    def __getitem__(self, idx):
+        while True:
+            try:
+                pixel_values, pixel_values_pose, clip_ref_image, pixel_values_ref_img = self.get_batch(idx)
+                break
+            except Exception as e:
+                idx = random.randint(0, self.length-1)
+        
+        pixel_values = self.pixel_transforms(pixel_values)
+        pixel_values_pose = self.pixel_transforms(pixel_values_pose)
+        
+        pixel_values_ref_img = pixel_values_ref_img.unsqueeze(0)
+        pixel_values_ref_img = self.pixel_transforms(pixel_values_ref_img)
+        pixel_values_ref_img = pixel_values_ref_img.squeeze(0)
+        
+        # clip_ref_image = clip_ref_image.unsqueeze(1) # [bs,1,768]
+
+        sample = dict(
+            pixel_values=pixel_values, 
+            pixel_values_pose=pixel_values_pose,
+            clip_ref_image=clip_ref_image,
+            pixel_values_ref_img=pixel_values_ref_img
+            )
+        
+        return sample
+
+
+
 
 # https://github.com/tencent-ailab/IP-Adapter/blob/main/tutorial_train.py#L341
 
@@ -146,19 +260,7 @@ def collate_fn(data):
         "pixel_values_ref_img": pixel_values_ref_img
     }
 
-# # ip-adapter collate_fn
-# def collate_fn(data):
-#     images = torch.stack([example["image"] for example in data])
-#     text_input_ids = torch.cat([example["text_input_ids"] for example in data], dim=0)
-#     clip_images = torch.cat([example["clip_image"] for example in data], dim=0)
-#     drop_image_embeds = [example["drop_image_embed"] for example in data]
 
-#     return {
-#         "images": images,
-#         "text_input_ids": text_input_ids,
-#         "clip_images": clip_images,
-#         "drop_image_embeds": drop_image_embeds
-#     }
 
 
 if __name__ == "__main__":
--- data/make_csv.py
+++ data/make_csv.py
@@ -4,6 +4,14 @@ import csv
 dataset_folder = '../../TikTok_dataset'
 csv_path = 'TikTok_info.csv'
 
+dataset_folder = '../../UBC_dataset'
+
+dataset_folder = '../../UBC_dataset/train'
+csv_path = 'UBC_train_info.csv'
+
+dataset_folder = '../../UBC_dataset/test'
+csv_path = 'UBC_test_info.csv'
+
 with open(csv_path, 'w', newline='') as csvfile:
     fieldnames = ['folder_id', 'folder_name']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
@@ -11,17 +19,17 @@ with open(csv_path, 'w', newline='') as csvfile:
     writer.writeheader()
     for i, folder in enumerate(os.listdir(dataset_folder)):
         folder_path = os.path.join(dataset_folder, folder)
-        writer.writerow({'folder_id': i+1, 'folder_name': folder})
+        writer.writerow({'folder_id': i+1, 'folder_name': folder.split(".")[0]})
 
-def test(csv_path):
-    with open(csv_path, 'r') as csvfile:
-        dataset = list(csv.DictReader(csvfile))
+# def test(csv_path):
+#     with open(csv_path, 'r') as csvfile:
+#         dataset = list(csv.DictReader(csvfile))
 
-    idx = 0  
-    video_dict = dataset[idx]
-    folder_id, folder_name = video_dict['folder_id'], video_dict['folder_name']
+#     idx = 0  
+#     video_dict = dataset[idx]
+#     folder_id, folder_name = video_dict['folder_id'], video_dict['folder_name']
 
-    print(video_dict)
-    print(len(dataset))
+#     print(video_dict)
+#     print(len(dataset))
 
-test(csv_path==csv_path)
+# test(csv_path==csv_path)
