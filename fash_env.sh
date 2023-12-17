a8e2b68da54d8330ff2a49ca89b3c774a894bf87
qguopku <55274489+guoqincode@users.noreply.github.com>
2023-12-17 16:16:24 +0800

    Create fash_env.sh

---
+++ fash_env.sh
@@ -0,0 +1,21 @@
+conda create -n animate python=3.8.18
+conda activate animate
+pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu117
+pip install diffusers==0.21.4
+pip install transformers==4.32.0
+pip install tqdm==4.66.1
+pip install omegaconf==2.3.0
+pip install einops==0.6.1
+conda install git-lfs
+pip install opencv-python==4.8.0.76
+pip install Pillow==9.5.0
+pip install safetensors==0.3.3
+pip install decord==0.6.0
+pip install wandb==0.16.1
+pip install accelerate==0.22.0
+
+cd pretrained_models
+git lfs install
+git clone https://huggingface.co/runwayml/stable-diffusion-v1-5
+git clone https://huggingface.co/openai/clip-vit-base-patch32
+# git clone https://huggingface.co/openai/clip-vit-large-patch14
