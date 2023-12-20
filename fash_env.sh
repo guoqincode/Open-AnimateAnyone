7033dd75223c21a95a3b191a5ad7b2264aa382a0
qguopku <55274489+guoqincode@users.noreply.github.com>
2023-12-20 22:42:10 +0800

    Update fash_env.sh

--- fash_env.sh
+++ fash_env.sh
@@ -6,7 +6,6 @@ pip install transformers==4.32.0
 pip install tqdm==4.66.1
 pip install omegaconf==2.3.0
 pip install einops==0.6.1
-conda install git-lfs
 pip install opencv-python==4.8.0.76
 pip install Pillow==9.5.0
 pip install safetensors==0.3.3
@@ -17,6 +16,8 @@ pip install imageio==2.9.0
 pip install av==11.0.0
 pip install imageio-ffmpeg
 
+# If you need to download the model locally:
+conda install git-lfs
 cd pretrained_models
 git lfs install
 git clone https://huggingface.co/runwayml/stable-diffusion-v1-5
