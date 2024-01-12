d11482c9ea4f77205058e64fee4443eb79f13ad2
qguopku <55274489+guoqincode@users.noreply.github.com>
2024-01-12 17:49:58 +0800

    Update hack_poseguider.py

--- models/hack_poseguider.py
+++ models/hack_poseguider.py
@@ -11,32 +11,24 @@ class Hack_PoseGuider(nn.Module):
 
         self.conv_layers = nn.Sequential(
             nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, padding=1),
-            nn.BatchNorm2d(3),
-            nn.ReLU(),
+            nn.SiLU(),
             nn.Conv2d(in_channels=3, out_channels=16, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(16),
-            nn.ReLU(),
+            nn.SiLU(),
 
             nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, padding=1),
-            nn.BatchNorm2d(16),
-            nn.ReLU(),
+            nn.SiLU(),
             nn.Conv2d(in_channels=16, out_channels=32, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
+            nn.SiLU(),
 
             nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
+            nn.SiLU(),
             nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
+            nn.SiLU(),
 
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
+            nn.SiLU(),
             nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU()
+            nn.SiLU()
         )
 
         # Final projection layer
